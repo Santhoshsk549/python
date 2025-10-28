803ef1621219ffc94f526e3d2481481232a96bc5
Santhoshsk549 <159749234+Santhoshsk549@users.noreply.github.com>
2025-10-28 17:34:41 +0530

    uses of variables

---
+++ variables.py
@@ -0,0 +1,33 @@
+name="santhoshkumar"
+age= 23
+height= 5.4
+weight= 60.5
+city="Madurai"
+isstudent= "false"
+mark="500"
+schoolname="govt hr sec school"
+pi=3.14
+mytext="Hello"
+print("name:",name)
+print(type(name))
+print("age:",age)
+print(type(age))
+print("weight:",height)
+print(type(height))
+print("weight:",weight)
+print(type(weight))
+print("city:",city)
+print(type(city))
+print("isstudent:",isstudent)
+print(type(isstudent))
+print("mark:",mark)
+print(type(mark))
+print("schoolname:",schoolname)
+print(type(schoolname))
+print("pi:",pi)
+print(type(pi))
+print("mytext:",mytext)
+print(type(mytext))
+
+
+
